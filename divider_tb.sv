ab45b2e067c14e2d405a334cdb760588be82dc90
Vikram Manja <vmanja@purdue.edu>
2017-08-25 00:22:09 -0700

    Create divider_tb.sv

---
+++ divider_tb.sv
@@ -0,0 +1,28 @@
+module divider_tb();
+
+  logic tb_clk;
+  logic tb_by_2, tb_by_3;
+  logic tb_nrst;
+  
+// CLOCKING
+  always begin
+  	tb_clk =   1'b0;
+    #5;
+    tb_clk = 1'b1;
+    #5;
+  end
+  
+  // Module instantiation
+  divider DUT ( .clk(tb_clk), .nrst(tb_nrst), .out2(tb_by_2), .out3(tb_by_3));
+  
+  initial begin
+    $dumpfile("dump.vcd"); 
+      $dumpvars;
+      tb_nrst = 1;
+      tb_nrst = 0;
+      #5;
+      tb_nrst = 1;
+      #40;
+      $finish;
+  end
+endmodule
